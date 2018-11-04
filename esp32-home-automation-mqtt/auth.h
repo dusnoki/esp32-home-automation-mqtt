35ea5d27a6d80f848c63dfc4a69556572b95bcbd
Boris Dusnoki <dusnoki@gmail.com>
2018-11-04 17:06:23 +0100

    Create auth.h

---
+++ esp32-home-automation-mqtt/auth.h
@@ -0,0 +1,5 @@
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+const char* serverHostname = "YOUR_RASPBERRY_PI_IP_ADDRESS";
+const String serverUsername = "admin";
+const String serverPassword = "password";
